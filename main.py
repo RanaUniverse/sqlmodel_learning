4676f2dab5b69e775e3eb7d15301ca8138402b58
RanaUniverse <RanaUniverse321@gmail.com>
2026-04-17 23:39:41 +0530

    now only making the tables

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-import sqlmodel
+from sqlmodel import Field, SQLModel, create_engine
 
-print(sqlmodel.__version__)
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: int | None = None
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+
+engine = create_engine(
+    url=sqlite_url,
+    echo=True,
+)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+if __name__ == "__main__":
+    create_db_and_tables()
