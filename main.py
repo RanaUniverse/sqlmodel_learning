8a0bf2d734dd44f49630c958167b580e14f344e9
RanaUniverse <RanaUniverse321@gmail.com>
2026-04-18 00:27:00 +0530

    now here the data is adding in the sqlite database by runnign this

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, Session, SQLModel, create_engine
 
 
 class Hero(SQLModel, table=True):
@@ -18,9 +18,45 @@ engine = create_engine(
 )
 
 
+def create_heroes():
+    hero_1 = Hero(name="Deadpond", secret_name="Dive wilson the bosy")
+    hero_2 = Hero(name="spider boy", secret_name="Pedro paraueido")
+    hero_3 = Hero(name="Rusty Man", secret_name="TOmmy sharp", age=48)
+
+    session = Session(bind=engine)
+
+    # session.add(hero_1)
+    # session.add(hero_2)
+    # session.add(hero_3)
+    # session.commit()
+    # session.close()
+
+    with Session(engine) as session:
+        session.add(hero_1)
+        session.add(hero_2)
+        session.add(hero_3)
+        print("After adding to the session:")
+        print("Hero 1", hero_1)
+        print("Hero 2", hero_2)
+        print("Hero 3", hero_3)
+        session.commit()
+        session.refresh(hero_1)
+        session.refresh(hero_2)
+        session.refresh(hero_3)
+        print("After adding to the commit fully:")
+        print("Hero 1", hero_1)
+        print("Hero 2", hero_2)
+        print("Hero 3", hero_3)
+
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 
-if __name__ == "__main__":
+def main():
     create_db_and_tables()
+    create_heroes()
+
+
+if __name__ == "__main__":
+    main()
